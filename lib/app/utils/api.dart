9ddef21933cb15994d9013cc6d5c95aadd0245f0
izzanghani <izzanghani07@gmail.com>
2025-03-12 20:04:57 +0700

    first commit

---
+++ lib/app/utils/api.dart
@@ -0,0 +1,5 @@
+class BaseUrl {
+  static String login = 'http://192.168.167.120:8000/api/login';
+  static String register = 'http://192.168.167.120:8000/api/register';
+  static String logout = 'http://192.168.167.120:8000/api/logout';
+}
